9bad35f67f42f16dacfdb42c4b40a1dea8d81ed6
admin <admin@Bcalab4-m15>
2022-06-15 09:41:35 +0530

    Commite first

---
+++ CaseToolExam/src/Secret_Locker_210.java
@@ -0,0 +1,18 @@
+
+public class Secret_Locker_210 {
+	
+		int secret_key;
+		Secret_Locker_210(int secret_key)
+		
+		   {
+			 this.secret_key=secret_key; 
+		   }
+		
+	         
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+		}
+
+	
+	}
